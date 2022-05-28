5b45f11540e505f706f0dca67be11e1bd2e201e1
Felipe Holanda <93724958+Felipe-Holanda@users.noreply.github.com>
2022-05-28 14:44:15 -0300

    Adicionando os arquivos ao repositório

---
+++ README.md
@@ -0,0 +1,42 @@
+# Pedra Papel e Tesoura!
+
+O clássico joguinho pra disputar quem vai ter a vez com seus amigos, só que agora em JavaScript!
+
+
+**Feito por mim:** Felipe Holanda
+<div style="">
+<a title="OBS: Não é referente a trabalho!" href='http://instagram.com/felipe.nether'><img src="https://img.shields.io/badge/Instagram-purple?style=for-the-badge&logo=instagram&logoColor=white"></img></a>
+<a title="OBS: Não é referente a trabalho!" href='http://twitter.com/felipe_nether'><img src="https://img.shields.io/badge/Twitter-darkblue?style=for-the-badge&logo=Twitter&logoColor=white"></img></a>
+<a href='https://api.whatsapp.com/send/?phone=5599982638444&text=Ol%C3%A1%2C+gostaria+de+efetuar+um+or%C3%A7amento&app_absent=0' title="ATENÇÃO: WhatsApp para trabalho apenas."><img src="https://img.shields.io/badge/WhatsApp-darkgreen?style=for-the-badge&logo=WhatsApp&logoColor=white"></img></a>
+<a href='mailto:felipe_nether@icloud.com' title="ATENÇÃO: Email para trabalho apenas."><img src="https://img.shields.io/badge/EMail-deepskyblue?style=for-the-badge&logo=gmail&logoColor=white"></img></a>
+</div>
+
+## Screenshots:
+
+> Como é:
+
+![Imagem representando a tela, qual exibe o conteúdo final deste repositório](https://i.imgur.com/qgxaf3S.png)
+## Sobre:
+
+Criei esta calculadora para testar a geração de números pseudo aleatórios do JavaScript, que se utiliza através do `Math.random()`
+
+### Screenshots do código:
+
+![Exibindo como utilizei o método Math.random](https://i.imgur.com/PxUgIpW.png)
+>Explicação:
+
+Atribuí a variável `sorteio` o `Math.floor(Math.random() *  3);`
+Desta forma, eu defini que: a propriedade `Math.Floor` me retornasse um número, que seria um número aleatório até 3... (ou seja, 0, 1 ou 2) e a eles atribuí a identificação visual, já que os pseudo aleatórios representariam as escolhas do computador.
+
+Após, eu apenas fiz a comparação, aninhando `IFs` e exibindo o resultado com um `<span>` na tela.
+
+> Comparadores que verificam e alteram o placar.
+![Aninhamento de if que verificam e alteram o placar.](https://i.imgur.com/Nvu0Ghw.png)
+#
+> Funções
+
+E claro, as funções que alteram os pontos no placar, somando ou redefinindo.
+
+![Aqui está uma imagem com as funções utilizadas ](https://i.imgur.com/wS0D15u.png)
+
+### Se gostou, sinta-se a vontade para baixar o código e estudar ou editar da maneira que preferir! <3
---
+++ assets/scripts/jokenpo.js
@@ -0,0 +1,81 @@
+let placar = [0, 0];
+
+function ganhou() {
+    placar[0]++;
+    document.getElementById('jogadorPontos').innerHTML = placar[0];
+    document.getElementById('notifica-resultado').innerHTML = 'Você ganhou!';
+    document.getElementById('notifica-resultado').setAttribute('class', 'ganhou');
+    Sleep(2000);
+    document.getElementById('notifica-resultado').setAttribute('class', 'esconde');
+
+}
+
+function perdeu() {
+    placar[1]++;
+    document.getElementById('pcPontos').innerHTML = placar[1];
+    document.getElementById('notifica-resultado').innerHTML = "Você perdeu ;(";
+    document.getElementById('notifica-resultado').setAttribute('class', 'perdeu');
+    Sleep(2000);
+    document.getElementById('notifica-resultado').setAttribute('class', 'esconde');
+}
+
+function empate() {
+    document.getElementById('notifica-resultado').innerHTML = "Empate!";
+    document.getElementById('notifica-resultado').setAttribute('class', 'empate');
+    Sleep(2000);
+    document.getElementById('notifica-resultado').setAttribute('class', 'esconde');
+}
+
+function reiniciar() {
+    placar = [0, 0];
+    document.getElementById('pc').src = "assets/img/pc.png";
+    document.getElementById('jogadorPontos').innerHTML = placar[0];
+    document.getElementById('pcPontos').innerHTML = placar[1];
+    document.getElementById('notifica-resultado').setAttribute('class', 'esconde');
+}
+
+function jogar() {
+    if (document.getElementById('Pedra').checked == false &&
+        document.getElementById('Papel').checked == false &&
+        document.getElementById('Tesoura').checked == false) {
+        alert("Para jogar, você deve selecionar uma opção!");
+    } else {
+        let sorteio = Math.floor(Math.random() * 3);
+        switch (sorteio) {
+            case 0:
+                document.getElementById('pc').src = "assets/img/pc_pedra.png";
+                break;
+            case 1:
+                document.getElementById('pc').src = "assets/img/pc_papel.png";
+                break;
+            case 2:
+                document.getElementById('pc').src = "assets/img/pc_tesoura.png";
+                break;
+        }
+        //empates
+        if (document.getElementById('Pedra').checked == true && sorteio == 0) {
+            empate();
+        } else if (document.getElementById('Papel').checked == true && sorteio == 1) {
+            empate();
+        } else if (document.getElementById('Tesoura').checked == true && sorteio == 2) {
+            empate();
+        }
+        //perca
+        else if (document.getElementById('Pedra').checked == true && sorteio == 1) {
+            perdeu();
+        } else if (document.getElementById('Papel').checked == true && sorteio == 2) {
+            perdeu();
+        } else if (document.getElementById('Tesoura').checked == true && sorteio == 0) {
+            perdeu();
+        }
+        //ganha
+        else if (document.getElementById('Pedra').checked == true && sorteio == 2) {
+            ganhou();
+        } else if (document.getElementById('Papel').checked == true && sorteio == 0) {
+            ganhou();
+        } else if (document.getElementById('Tesoura').checked == true && sorteio == 1) {
+            ganhou();
+        }
+    }
+}
+
